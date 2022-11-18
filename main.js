3f0044a1d872bc68a451934a2702044ad734013d
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-18 15:42:02 +0100

    creazione numeri random

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 // 5 NUMERI CASUALI IN PAGINA
 // TIMER 30 SECONDI
 // DOPO 30 SEC, I NUMERI SCOMPAIONO E L'UTENTE DOVRA' INSERIRE I NUMERI UNO ALLA VOLTA.
+
+const btnstart = document.getElementById("start");
+const RandNum = document.getElementById("numbers");
+
+
+btnstart.addEventListener("click",
+
+    function() {
+        let ArrNumber = ArrNumbRandom(5,1,10);
+        console.log(ArrNumber);
+        RandNum.innerHTML = `<div>${ArrNumber}</div>`
+    }
+
+);
