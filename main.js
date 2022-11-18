7cd097ecb6f3eb6ac7b1bb4a59af2ca5dac8f4f6
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-18 17:09:00 +0100

    array num random creata

--- main.js
+++ main.js
@@ -6,12 +6,32 @@ const btnstart = document.getElementById("start");
 const RandNum = document.getElementById("numbers");
 
 
+function ArrNumbRandom(min, max) {
+    return Math.floor(Math.random() * (50 - 1 + 1) ) + 1;
+}
+
+function generaMinMax(min,max){
+    return (Math.floor(Math.random() * ((max + 1) - min) + min))
+
+   }
+    function generaArrayNrRandom(quanti,numMin,numMax){
+        const ARRAYNUMERI=[];
+    while ((ARRAYNUMERI.length) < quanti){
+        let nuovoNumero= generaMinMax(numMin,numMax);
+        if ( !ARRAYNUMERI.includes(nuovoNumero)){
+            ARRAYNUMERI.push(nuovoNumero);
+        }
+    }
+    return ARRAYNUMERI
+}
+
 btnstart.addEventListener("click",
 
     function() {
-        let ArrNumber = ArrNumbRandom(5,1,10);
+        let ArrNumber = generaArrayNrRandom(5,1,50);
         console.log(ArrNumber);
-        RandNum.innerHTML = `<div>${ArrNumber}</div>`
+        RandNum.innerHTML += `<div>${ArrNumber}</div>`
     }
 
 );
+
