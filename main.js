ebc53cf52b2960936ce65b13e6cbadb9c548b48d
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-18 17:38:53 +0100

    problema timer

--- main.js
+++ main.js
@@ -33,5 +33,16 @@ btnstart.addEventListener("click",
         RandNum.innerHTML += `<div>${ArrNumber}</div>`
     }
 
+    setTimeout()( => {
+        const box = document.getElementById('numbers');
+      
+        // 👇️ removes element from DOM
+        box.style.display = 'none';
+      
+        // 👇️ hides element (still takes up space on page)
+        // box.style.visibility = 'hidden';
+      }, 3000); // 👈️ time in milliseconds
+      
+
 );
 
